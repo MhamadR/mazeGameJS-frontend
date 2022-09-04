b9ffa4f1a3c1abb23c5a32b1679634e364657c24
MhamadR <mhamadraslan@gmail.com>
2022-09-04 22:09:38 +0300

    Added lose(), win(), resetBoundaries(), resetStatus()

--- game.js
+++ game.js
@@ -10,6 +10,12 @@ function game() {
   const end = document.getElementById("end");
   const resultLog = document.querySelector(".example");
   const boundary1 = document.querySelector("#boundary1");
+  // When status is true, the game is paused until reset or restart
+  let status = false;
+
+  // Centering the score and the result
+  scoreLog.style.textAlign = "center";
+  resultLog.style.textAlign = "center";
 
   /* Creating, styling, and adding ".block" to block the user from reaching the end target from the body path */
   const node = document.createElement("div");
@@ -31,6 +37,53 @@ function game() {
   end.addEventListener("mouseover", win);
   for (let i = 0; i < boundary.length; i++) {
     boundary[i].addEventListener("mouseover", lose);
+  }
+
+  // Changing borders to red, decreasing and displaying the score
+  function lose() {
+    if (!status) {
+      status = true;
+      score -= 10;
+      scoreLog.innerText = "Your score: " + score;
+      boundary.forEach((element) => {
+        element.style.border = "1px solid red";
+      });
+      boundary[1].style.borderLeft = "none";
+      boundary[1].style.borderRight = "none";
+      boundary[4].style.borderBottom = "none";
+
+      // Adding the class ".youlose" to display the result in red bg
+      resultLog.classList.add("youlose");
+      resultLog.innerText = "You lose!";
+    }
+  }
+
+  // Resetting boundaries
+  function resetBoundaries() {
+    boundary.forEach((element) => {
+      element.style.border = "1px solid black";
+    });
+    boundary[1].style.borderLeft = "none";
+    boundary[1].style.borderRight = "none";
+    boundary[4].style.borderBottom = "none";
+  }
+
+  // Resetting borders, increasing and displaying the score
+  function win() {
+    if (!status) {
+      status = true;
+      score += 5;
+      scoreLog.innerText = score;
+      resultLog.innerText = "You win!";
+      resetBoundaries();
+    }
+  }
+
+  // Resetting result, status, and boundaries
+  function resetStatus() {
+    status = false;
+    resultLog.innerText = "";
+    resultLog.classList.remove("youlose");
   }
 }
 
