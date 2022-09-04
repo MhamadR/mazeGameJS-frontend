e9d55ecb075577d898bc05fb63281d5d95beeea2
MhamadR <mhamadraslan@gmail.com>
2022-09-04 21:44:09 +0300

    Added variables to JS

---
+++ game.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", game);
+
+/* Start of: function to be fired after the page has loaded */
+function game() {
+  // Setting up variables and assigning elements
+  let score = 0;
+  const scoreLog = document.querySelector("#status");
+  const boundary = document.querySelectorAll("#game .boundary");
+  const start = document.getElementById("start");
+  const end = document.getElementById("end");
+  const resultLog = document.querySelector(".example");
+  const boundary1 = document.querySelector("#boundary1");
+}
+
+/* End of: function to be fired after the page has loaded */
