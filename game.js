2367ddf5e6f6eac1a290f053a9ece17c81b15937
MhamadR <mhamadraslan@gmail.com>
2022-09-04 21:55:35 +0300

    Added event listeners

--- game.js
+++ game.js
@@ -11,8 +11,10 @@ function game() {
   const resultLog = document.querySelector(".example");
   const boundary1 = document.querySelector("#boundary1");
 
+  /* Creating, styling, and adding ".block" to block the user from reaching the end target from the body path */
   const node = document.createElement("div");
   node.classList.add("block");
+  boundary1.appendChild(node);
   node.style.cssText = `
   position: absolute;
   height: 50px;
@@ -21,6 +23,15 @@ function game() {
   left: 500px;
   opacity: 0;
   `;
+
+  // Adding event listeners
+  start.addEventListener("mouseover", resetBoundaries);
+  start.addEventListener("mouseover", resetStatus);
+  start.addEventListener("click", restart);
+  end.addEventListener("mouseover", win);
+  for (let i = 0; i < boundary.length; i++) {
+    boundary[i].addEventListener("mouseover", lose);
+  }
 }
 
 /* End of: function to be fired after the page has loaded */
